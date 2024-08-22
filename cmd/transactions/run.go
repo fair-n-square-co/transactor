eaae774cc74a8eb893238b44b952bc4d09cb8e64
Jaspreet Singh <findjsk@gmail.com>
2024-08-22 19:13:00 +1000

    ✨ Add REST API support (#55)

--- cmd/transactions/run.go
+++ cmd/transactions/run.go
@@ -1,17 +1,26 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
+	"net/http"
 	"os"
 
-	v1alpha1 "github.com/fair-n-square-co/apis/gen/pkg/fairnsquare/service/user/v1alpha1"
+	usergw "github.com/fair-n-square-co/apis/gen/pkg/fairnsquare/service/user/v1alpha1"
 	"github.com/fair-n-square-co/transactions/internal/cmd/transactions"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/reflection"
 )
 
-func run() {
+func run() error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
 	// get PORT from env variables
 	// if not set, use default port
 	port := ":8080"
@@ -39,11 +48,37 @@ func run() {
 		log.Fatalf("failed to create server: %v", err)
 	}
 
-	v1alpha1.RegisterGroupServiceServer(grpcServer, serverGroup.GroupServer)
-	v1alpha1.RegisterUserServiceServer(grpcServer, serverGroup.UserServer)
+	usergw.RegisterGroupServiceServer(grpcServer, serverGroup.GroupServer)
+	usergw.RegisterUserServiceServer(grpcServer, serverGroup.UserServer)
 
-	log.Printf("listening on port %s", port)
-	if err = grpcServer.Serve(lis); err != nil {
-		log.Fatalf("Failed at: %v", err)
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	err = usergw.RegisterUserServiceHandlerFromEndpoint(ctx, mux, port, opts)
+	if err != nil {
+		return err
 	}
+
+	err = usergw.RegisterGroupServiceHandlerFromEndpoint(ctx, mux, port, opts)
+	if err != nil {
+		return err
+	}
+
+	g, _ := errgroup.WithContext(ctx)
+
+	g.Go(func() error {
+		log.Printf("starting http server on port %s", ":8081")
+		return http.ListenAndServe(":8081", mux)
+	})
+
+	g.Go(func() error {
+		log.Printf("listening on port %s", port)
+		return grpcServer.Serve(lis)
+	})
+
+	if err := g.Wait(); err != nil {
+		log.Fatalf("server error: %v", err)
+		return err
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -8,7 +8,9 @@ require (
 	github.com/fair-n-square-co/apis/gen/pkg/fairnsquare/service/user/v1alpha1 v0.2.0
 	github.com/fair-n-square-co/apis/gen/pkg/fairnsquare/type/user/v1alpha1 v0.1.0
 	github.com/google/uuid v1.6.0
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.21.0
 	go.uber.org/mock v0.4.0
+	golang.org/x/sync v0.7.0
 	google.golang.org/genproto v0.0.0-20240415180920-8c6c420018be
 	google.golang.org/grpc v1.65.0
 	gorm.io/driver/postgres v1.5.7
@@ -20,7 +22,6 @@ require (
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
 	github.com/golang-sql/sqlexp v0.1.0 // indirect
-	github.com/grpc-ecosystem/grpc-gateway/v2 v2.21.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20231201235250-de7065d80cb9 // indirect
 	github.com/jackc/pgx/v5 v5.5.5 // indirect
@@ -32,7 +33,6 @@ require (
 	github.com/stretchr/testify v1.9.0 // indirect
 	golang.org/x/crypto v0.24.0 // indirect
 	golang.org/x/net v0.26.0 // indirect
-	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20240808171019-573a1156607a // indirect
